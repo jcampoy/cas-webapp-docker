2896e8d4440bd332fec59215d7de94972464106c
Jose A. Jimenez <jose.jimenez@liferay.com>
2018-09-07 13:17:41 +0200

    use other ports and ask for the docker user

--- run.sh
+++ run.sh
@@ -4,15 +4,17 @@ docker rm cas -f &>/dev/null
 
 cas_version=$1
 
+read -p "Docker user: " docker_user;
+
 if [ $# -eq 0 ] || [ -z "$cas_version" ]
   then
     echo "No CAS version is specified."
-    read -p "Provide a tag for the CAS image you are about to run (i.e. 5.0.0): " cas_version;
+    read -p "Provide a tag for the CAS image you are about to run (i.e. 5.3.2): " cas_version;
 fi
 
 if [ ! -z "$cas_version" ]
   then
-	docker run -d -p 8080:8080 -p 8443:8443 --name="cas" apereo/cas:v$cas_version
+	docker run -d -p 9090:8080 -p 9443:8443 --name="cas" $docker_user/cas:v$cas_version
 	docker logs -f cas
   else
   	echo "No image tag is provided."	
